a9277801b04c58e557bc4947d0fe94988446b1a1
17193 <asialevesquegault@gmail.com>
2021-01-13 12:52:28 -0700

    Add project files.

---
+++ LargerOfTwoNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LargerOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Write a program which takes two numbers from the console and
+           // displays the larger of the two.
+
+           //Cw Input first number:
+           Console.WriteLine("Please input first number:");
+           //var of cr
+           var firstNumber = Convert.ToInt32(Console.ReadLine());
+           //Cw input Second number:
+           Console.WriteLine("Please input second number:");
+            //var of cr
+            var secondNumber = Convert.ToInt32(Console.ReadLine());
+            //if first>2nd return: First lrger
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine("First number is bigger!");
+            }
+            else if (firstNumber<secondNumber)
+            {
+                Console.WriteLine("Second number is bigger!");
+            }
+            else
+            {
+                Console.WriteLine("THEY ARE EQUAL!");
+            }
+            //else second larger
+        }
+    }
+}
